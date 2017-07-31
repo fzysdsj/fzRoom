1b817619d8abfe355807b769d816d3a9e6e15f61
unknown <>
2017-08-01 00:11:44 +0800

    666

--- controllers/articles.js
+++ controllers/articles.js
@@ -6,15 +6,29 @@ var fs = require('fs');
 var path = require('path');
 // var formidable = require('formidable');
 var router = express.Router();
-router.get("/", function (req, res, next) {
-    // db.query("select * from article", function (err, rows) {
-    //     if (err) {
-    //         res.render("userList", { title: "用户列表", datas: [] });
-    //     } else {
-    //         res.render("userList", { title: "用户列表", datas: rows });
-    //     }
-    // });
-    res.render('article',{title:'文章页'});
+router.get("/",function(req,res,next){
+        db.query("select * from article", function (err, rows) {
+        if (err) {
+            res.render("articleList", { title: "用户列表", datas: [] });
+        } else {
+            res.render("articleList", { title: "用户列表", datas: rows });
+        }
+    });
+})
+router.get("/:id", function (req, res, next) {
+   var id = req.params.id;
+    console.log("文章ID："+id);
+    var sql = "select * from article where artId = " + id;
+    console.log(sql);
+    db.query(sql, function (err, rows) {
+        if (err) {
+            res.send("修改页面跳转失败");
+        } else {
+            console.log("文章发表成功");
+            res.render("articleId", { datas: rows });
+            res.redirect("/articles/"+id);
+        }
+    });
 });
 
 //添加用户
@@ -22,9 +36,10 @@ router.get("/add", function (req, res, next) {
     res.render("artAdd");
 });
 router.post("/add", function (req, res, next) {
+    var id = req.params.id;
+    console.log("文章ID："+id);
     var arttitle = req.body.arttitle;
     var artcontent = req.body.artcontent;
-    var artuid = req.body.artuid;
     var artuid = req.body.artuid;
     console.log("arttitle:"+arttitle);
     console.log("artcontent:"+artcontent);
